f3345c10757b77bb1551db796e9db7465420ae00
Sekai Strawberry <fran.castrobar@gmail.com>
2021-06-16 02:53:09 -0400

    se lee archivo markdown

---
+++ READMETestLinks.md
@@ -0,0 +1,9 @@
+[link a facebook] (http://www.facebook.com)
+
+[link roto] (http://www.fe1bun.com)
+
+https://i.ytimg.com/vi/gKNJsR4EdqI/maxresdefault.jpg
+
+https://www.googl1234e.com/search?q=dia+del+platano+shi+sheñol&client=firefox-b-d&sxsrf=ALeKk03PKLhqN1rOgkS2RNjLewo-w9NS8w:1623796501110&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjz8Iqt2ZrxAhUJKLkGHVt8A5gQ_AUoAXoECAEQAw&biw=1366&bih=615#imgrc=iS-nuhPHP_QDpM
+
+https://www.google.com/search?q=dia+del+platano+shi+sheñol&client=firefox-b-d&sxsrf=ALeKk03PKLhqN1rOgkS2RNjLewo-w9NS8w:1623796501110&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjz8Iqt2ZrxAhUJKLkGHVt8A5gQ_AUoAXoECAEQAw&biw=1366&bih=615#imgrc=iS-nuhPHP_QDpM
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-module.exports = () => {
-  // ...
-};
+//Librerías
+const fs = require('fs');
+const readline = require("readline");
+const textFile = 'READMETestLinks.md'
+
+//LEER ARCHIVO MD LÍNEA POR LÍNEA
+const readLinks = () => {
+  return new Promise ((resolve, reject) => {  //Promesa para tarea asíncrona
+    let reader = readline.createInterface({     //Crear lector para leer archivo por líneas
+      input: fs.createReadStream(textFile)
+    });
+    const regex = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ig  //Expresion regular, patrón para encontrar/separar secuencias de caracteres
+    let linePosition = 0; 
+    reader.on("line", textLine => {  //Al encontrar una línea, damos insctrucciones
+      if (textLine !== "") {
+      // console.log("Esta línea dice:", textLine);
+      const linksOnLines = [];  //Declaramos un array para trabajar con él
+      linePosition++;
+      const separateURL = textLine.matchAll(regex);
+        for (const match of separateURL) {
+          const objectLink = {
+            line: linePosition,
+            file: textFile,
+            link: match[0]
+          }
+          // console.log(objectLink.file);
+        linksOnLines.push(objectLink);
+        console.log(linksOnLines) 
+      }
+      }
+  })
+  });  
+}
+
+readLinks();
+
+const linksStatus = () => {
+  return new Promise ((resolve, reject) => {
+    //ASDFASDAF
+
+  })
+}
+
+// module.exports = () => {
+//   // ...
+// };
+
